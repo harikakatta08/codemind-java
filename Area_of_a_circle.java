a0ae10c85c98789eb903c5ec030bb6c4f0b2649a
harikakatta08 <143778983+harikakatta08@users.noreply.github.com>
2023-09-01 11:37:51 +0530

    import java.util.Scanner; public class sum {     public static void main(String[] args)     {         Scanner sc=new Scanner(System.in);         double area;     int r;         r=sc.nextInt();         area=3.14*r*r;         System.out.printf("%.2f",area);     } }

---
+++ Area_of_a_circle.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class sum
+{
+    public static void main(String[] args)
+    {
+        Scanner sc=new Scanner(System.in);
+        double area;
+    int r;
+        r=sc.nextInt();
+        area=3.14*r*r;
+        System.out.printf("%.2f",area);
+    }
+}
